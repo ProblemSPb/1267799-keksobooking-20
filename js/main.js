8810f8e5b88e410db832c089c881632474580910
ProblemSPb <ussr83@mail.ru>
2020-06-14 23:30:20 +0200

    Личный проект: доверяй, но проверяй (часть 1)

--- js/main.js
+++ js/main.js
@@ -48,6 +48,8 @@ var COORDINATE_X_MAX = 1140;
 var COORDINATE_Y_MIN = 130;
 var COORDINATE_Y_MAX = 630;
 var PIN_SIZE = [50, 70];
+// пин-маффин
+var SEARCH_PIN_SIZE = [65, 65];
 
 // Получение рандомного числа в заданном интервале. Максимум и минимум включаются
 function getRandomIntInclusive(min, max) {
@@ -96,12 +98,6 @@ function generateAds(maxAdsNumber) {
   return ads;
 }
 
-// переключение карту в активное состояние
-// убираем класс .map--faded
-var map = document.querySelector('.map');
-
-map.classList.remove('map--faded');
-
 var ads = generateAds(NUMBER_OF_ADS);
 
 var templatePin = document.querySelector('#pin').content.querySelector('.map__pin');
@@ -121,10 +117,127 @@ function renderPin(ads) {
   return pin;
 }
 
-for (var i = 0; i < NUMBER_OF_ADS; i++) {
-  fragment.appendChild(renderPin(ads[i]));
+var map = document.querySelector('.map');
+
+// определяет расположение пина
+var getPosition = function (pin) {
+  var top = pin.style.top;
+  var left = pin.style.left;
+
+  // избавляемся от px в конце и переводим в int
+  top = parseInt(top.slice(0, -2))
+  left = parseInt(left.slice(0, -2));
+
+  // определяем середину для X координаты
+  var xCenter = Math.round(left - SEARCH_PIN_SIZE[0] / 2)
+  // и для Y если неактивен
+  var yCenter = Math.round(top - SEARCH_PIN_SIZE[1] / 2);
+  // Y если пин активен
+  if (!(map.classList.contains('map--faded'))) {
+    yCenter = Math.round(top - (SEARCH_PIN_SIZE[1] + 22) / 2);
+  }
+
+  return xCenter + ', ' + yCenter;
 }
-listElement.appendChild(fragment);
+
+// Пин по дифолту
+var defaultSerachPin = document.querySelector('.map__pin--main');
+// Адрес по дифолту
+var addressField = document.querySelector('#address');
+addressField.value = getPosition(defaultSerachPin);
+
+var fieldset = document.querySelector('fieldset');
+var adForm = document.querySelector(".ad-form")
+var adFormFieldset = adForm.querySelector('fieldset');
+var adFormMapFilters = document.querySelectorAll('.map__filter');
+
+// при открытии страницы все элементы кроме пина должны быть неактивны
+var deactivatePage = function (map, fieldset, adFormFieldset, adFormMapFilters) {
+  map.classList.add('map--faded');
+  fieldset.disabled = true;
+  adFormFieldset.disabled = true;
+
+  for (var filter of adFormMapFilters) {
+    filter.disabled = true;
+  }
+}
+
+deactivatePage(map, fieldset, adFormFieldset, adFormMapFilters);
+
+// активирует страницу
+var activatePage = function () {
+  map.classList.remove('map--faded');
+  fieldset.disabled = false;
+  adFormFieldset.disabled = false;
+  adForm.classList.remove('ad-form--disabled');
+
+  for (var filter of adFormMapFilters) {
+    filter.disabled = false;
+  }
+
+  for (var i = 0; i < NUMBER_OF_ADS; i++) {
+    fragment.appendChild(renderPin(ads[i]));
+  }
+  listElement.appendChild(fragment);
+
+  // асайниит новый адрес пину после активации
+  addressField.value = getPosition(defaultSerachPin);
+}
+
+// если нажата левая кнопка мыши
+var clickPin = document.querySelector('.map__pin--main');
+clickPin.addEventListener('mousedown', function (evt) {
+  if (typeof evt === 'object' && evt.button === 0) {
+    activatePage();
+  }
+  // TODO: когда удалять этот обработчик?
+});
+
+// если нажат enter
+clickPin.addEventListener('keyup', function (evt) {
+  if (evt.keyCode === 13) {
+    activatePage();
+  }
+  // TODO: когда удалять этот обработчик?
+});
+
+// Назначение кастомного значения попапу с ошибкой валидации полей формы
+// Поля: "Количество комнат" и "Количество мест"
+var validateRoomsGuests = function (field) {
+  if (selectedNumberRooms == 1 && selectedNumberGuests != 1) {
+    field.setCustomValidity('1 комната должна быть для 1 гостя');
+  } else if (selectedNumberRooms == 2 && (selectedNumberGuests > 0 && selectedNumberGuests < 3)) {
+    field.setCustomValidity('2 комнаты должны быть для 1 или 2 гостей');
+  } else if (selectedNumberRooms == 3 && (selectedNumberGuests > 0 && selectedNumberGuests < 4)) {
+    field.setCustomValidity('3 комнаты должны быть для 1, 2 или 3 гостей');
+  } else if (selectedNumberRooms == 100 && (selectedNumberGuests != 0)) {
+    field.setCustomValidity('100 комнат должны быть не для гостей');
+  }
+}
+
+// дифолтовые значения
+var selectedNumberRooms = 1;
+var selectedNumberGuests = 3;
+
+var roomNumber = document.querySelector('#room_number');
+roomNumber.addEventListener('change', function () {
+  selectedNumberRooms = parseInt(this.value)
+
+  validateRoomsGuests(capacity);
+});
+
+var capacity = document.querySelector('#capacity');
+capacity.addEventListener('change', function () {
+  selectedNumberGuests = parseInt(this.value);
+
+  validateRoomsGuests(capacity);
+});
+
+var formSubmit = document.querySelector('.ad-form__submit');
+formSubmit.addEventListener('submit', function () {
+  // если не изменили дифолтовые значения полей "Количество комнат" и "Количество мест"
+  validateRoomsGuests(capacity);
+});
 
 // console.log(ads);
 // console.warn(map, map.getBoundingClientRect().x);
